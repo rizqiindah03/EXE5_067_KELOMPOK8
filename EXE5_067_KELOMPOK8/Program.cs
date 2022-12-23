816777a35ee87a049d9b14a10783aa990e52dc76
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-23 16:03:12 +0700

    Add project files.

---
+++ EXE5_067_KELOMPOK8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_067_KELOMPOK8
+{
+    class Node
+    {
+        public string name;
+        public Node next;    
+    }
+}
